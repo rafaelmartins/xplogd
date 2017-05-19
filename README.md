882117bf5e43b2015ee5dee9af0f8d7edd37b954
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2017-05-20 01:35:40 +0200

    optimized network usage by sending flight data only once.

--- README.md
+++ README.md
@@ -29,29 +29,51 @@ The user and password should be provided in the URL.
 ## Protocol
 
 ```c
-/* xplogd protocol, version 1
+/**
+ * xplogd protocol, version 1
  *
- * This is the definition of our protocol. all the fields are separated with
+ * This is the definition of our protocol. All the fields are separated with
  * a newline '\n' character.
  *
- * The server should return 202 to notify that accepted the data, and always
- * check if the client sent the correct content-type header
- * (application/vnd.xplogd.serialized).
- *
- * When a request is not accepted, the client may take some action to recover,
- * like stop sending for a while, or increase the send intervals.
+ * We have 2 types of requests, that are defined by the Content-Type header.
  */
-const char *body_format =
+
+/**
+ * Content-Type: application/vnd.xplogd.flight
+ *
+ * This request registers a new flight in the server. It just contains the
+ * aircraft data for now, but information about origin airport and destination
+ * aiport should be added in a new version of the protocol.
+ *
+ * The server should return 201 to notify that the new flight was registered,
+ * and the ID of the flight, that will be used by the upcoming requests.
+ */
+const char *flight_format =
     "1\n"   // protocol version
     "%s\n"  // aircraft icao
     "%s\n"  // aircraft tailnum
     "%s\n"  // aircraft description
-    "%f\n"  // latitude in degrees
-    "%f\n"  // longitude in degrees
-    "%f\n"  // altitude in meters (not feets!)
-    "%f\n"  // track in degrees
-    "%f\n"  // ground speed in meters per second (not knots!)
-    "%f\n"  // air speed in meters per second (not knots!)
-    "%f\n"  // vertical speed meters per second (not feets per minute!)
+    "";
+
+/**
+ * Content-Type: application/vnd.xplogd.position
+ *
+ * This request send information about the current status of the aircraft to
+ * the server.
+ *
+ * The server should return 202 to notify that accepted the data. When a
+ * request is not accepted, the client may take some action to recover, like
+ * stop sending for a while, or increase the send intervals.
+ */
+const char *position_format =
+    "1\n"     // protocol version
+    "%lld\n"  // flight id
+    "%f\n"    // latitude in degrees
+    "%f\n"    // longitude in degrees
+    "%f\n"    // altitude in meters (not feets!)
+    "%f\n"    // track in degrees
+    "%f\n"    // ground speed in meters per second (not knots!)
+    "%f\n"    // air speed in meters per second (not knots!)
+    "%f\n"    // vertical speed meters per second (not feets per minute!)
     "";
 ```
--- main.c
+++ main.c
@@ -12,11 +12,15 @@
 #include "sdk/XPLMProcessing.h"
 #include "sdk/XPLMUtilities.h"
 
-
 typedef struct {
     char aircraft_icao[40];
     char aircraft_tailnum[40];
     char aircraft_description[260];
+    // want to add origin and destination here too, but need to figure a simple
+    // way to ask users to input it.
+} flight_data_t;
+
+typedef struct {
     double latitude;
     double longitude;
     double altitude;
@@ -26,6 +30,10 @@ typedef struct {
     float vertical_speed;
 } position_data_t;
 
+typedef struct {
+    char *str;
+    size_t size;
+} str_buf_t;
 
 static XPLMDataRef *aircraft_icao_dr = NULL;
 static XPLMDataRef *aircraft_tailnum_dr = NULL;
@@ -39,38 +47,139 @@ static XPLMDataRef *air_speed_dr = NULL;
 static XPLMDataRef *vertical_speed_dr = NULL;
 
 static char url[1024];
-static bool sending = true;
+static bool sending = false;
+static long long int flight_id = -1;
 
 static int main_submenu;
 static XPLMMenuID status_menu;
 static int status_index;
 
 
-/* xplogd protocol, version 1
+/**
+ * xplogd protocol, version 1
  *
- * This is the definition of our protocol. all the fields are separated with
+ * This is the definition of our protocol. All the fields are separated with
  * a newline '\n' character.
  *
- * The server should return 202 to notify that accepted the data, and always
- * check if the client sent the correct content-type header
- * (application/vnd.xplogd.serialized).
- *
- * When a request is not accepted, the client may take some action to recover,
- * like stop sending for a while, or increase the send intervals.
+ * We have 2 types of requests, that are defined by the Content-Type header.
  */
-const char *body_format =
+
+/**
+ * Content-Type: application/vnd.xplogd.flight
+ *
+ * This request registers a new flight in the server. It just contains the
+ * aircraft data for now, but information about origin airport and destination
+ * aiport should be added in a new version of the protocol.
+ *
+ * The server should return 201 to notify that the new flight was registered,
+ * and the ID of the flight, that will be used by the upcoming requests.
+ */
+const char *flight_format =
     "1\n"   // protocol version
     "%s\n"  // aircraft icao
     "%s\n"  // aircraft tailnum
     "%s\n"  // aircraft description
-    "%f\n"  // latitude in degrees
-    "%f\n"  // longitude in degrees
-    "%f\n"  // altitude in meters (not feets!)
-    "%f\n"  // track in degrees
-    "%f\n"  // ground speed in meters per second (not knots!)
-    "%f\n"  // air speed in meters per second (not knots!)
-    "%f\n"  // vertical speed meters per second (not feets per minute!)
     "";
+
+/**
+ * Content-Type: application/vnd.xplogd.position
+ *
+ * This request send information about the current status of the aircraft to
+ * the server.
+ *
+ * The server should return 202 to notify that accepted the data. When a
+ * request is not accepted, the client may take some action to recover, like
+ * stop sending for a while, or increase the send intervals.
+ */
+const char *position_format =
+    "1\n"     // protocol version
+    "%lld\n"  // flight id
+    "%f\n"    // latitude in degrees
+    "%f\n"    // longitude in degrees
+    "%f\n"    // altitude in meters (not feets!)
+    "%f\n"    // track in degrees
+    "%f\n"    // ground speed in meters per second (not knots!)
+    "%f\n"    // air speed in meters per second (not knots!)
+    "%f\n"    // vertical speed meters per second (not feets per minute!)
+    "";
+
+
+static size_t
+SendFlightDataCallback(char *ptr, size_t size, size_t nmemb, void *userdata)
+{
+    size_t realsize = size * nmemb;
+    str_buf_t *buf = userdata;
+
+    buf->str = realloc(buf->str, buf->size + realsize + 1);
+    if (buf->str == NULL) {
+        printf("error: failed to realloc memory\n");
+        return 0;
+    }
+
+    memcpy(&(buf->str[buf->size]), ptr, realsize);
+    buf->size += realsize;
+    buf->str[buf->size] = '\0';
+
+    return realsize;
+}
+
+
+static bool
+SendFlightData(const char *url, flight_data_t *data)
+{
+    if (url == NULL || data == NULL)
+        return false;
+
+    int body_len = snprintf(NULL, 0, flight_format, data->aircraft_icao,
+        data->aircraft_tailnum, data->aircraft_description);
+
+    if (body_len <= 0)
+        return false;
+
+    char *body = malloc(body_len + 1);
+    if (body == NULL)
+        return false;
+
+    snprintf(body, body_len + 1, flight_format, data->aircraft_icao,
+        data->aircraft_tailnum, data->aircraft_description);
+
+    struct curl_slist *headers = NULL;
+    headers = curl_slist_append(headers,
+        "Content-Type: application/vnd.xplogd.flight");
+
+    str_buf_t buf;
+    buf.str = malloc(1);
+    buf.size = 0;
+
+    CURL *hnd = curl_easy_init();
+    curl_easy_setopt(hnd, CURLOPT_URL, url);
+    curl_easy_setopt(hnd, CURLOPT_WRITEFUNCTION, SendFlightDataCallback);
+    curl_easy_setopt(hnd, CURLOPT_WRITEDATA, (void *) &buf);
+    curl_easy_setopt(hnd, CURLOPT_NOPROGRESS, 1L);
+    curl_easy_setopt(hnd, CURLOPT_USERAGENT, PACKAGE_NAME "/" PACKAGE_VERSION);
+    curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
+    curl_easy_setopt(hnd, CURLOPT_MAXREDIRS, 50L);
+    curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, body);
+
+    bool rv = curl_easy_perform(hnd) == CURLE_OK;
+    if (rv) {
+        long http_code = 0;
+        curl_easy_getinfo(hnd, CURLINFO_RESPONSE_CODE, &http_code);
+        rv = http_code == 201;
+        if (rv) {
+            long long int tmp_id = strtoll(buf.str, NULL, 10);
+            if (tmp_id >= 1)
+                flight_id = tmp_id;
+        }
+    }
+
+    free(buf.str);
+    free(body);
+    curl_slist_free_all(headers);
+    curl_easy_cleanup(hnd);
+
+    return rv;
+}
 
 
 static bool
@@ -79,8 +188,7 @@ SendPositionData(const char *url, position_data_t *data)
     if (url == NULL || data == NULL)
         return false;
 
-    int body_len = snprintf(NULL, 0, body_format, data->aircraft_icao,
-        data->aircraft_tailnum, data->aircraft_description, data->latitude,
+    int body_len = snprintf(NULL, 0, position_format, flight_id, data->latitude,
         data->longitude, data->altitude, data->track, data->ground_speed,
         data->air_speed, data->vertical_speed);
 
@@ -91,14 +199,13 @@ SendPositionData(const char *url, position_data_t *data)
     if (body == NULL)
         return false;
 
-    snprintf(body, body_len + 1, body_format, data->aircraft_icao,
-        data->aircraft_tailnum, data->aircraft_description, data->latitude,
+    snprintf(body, body_len + 1, position_format, flight_id, data->latitude,
         data->longitude, data->altitude, data->track, data->ground_speed,
         data->air_speed, data->vertical_speed);
 
     struct curl_slist *headers = NULL;
     headers = curl_slist_append(headers,
-        "Content-Type: application/vnd.xplogd.serialized");
+        "Content-Type: application/vnd.xplogd.position");
 
     CURL *hnd = curl_easy_init();
     curl_easy_setopt(hnd, CURLOPT_URL, url);
@@ -181,40 +288,46 @@ str_strip(char *str)
 static float
 FlightLoopCallback(float elapsedMe, float elapsedSim, int counter, void *refcon)
 {
-    position_data_t *data = malloc(sizeof(position_data_t));
-    if (data == NULL)
-        goto cleanup;
-
-    XPLMGetDatab(aircraft_icao_dr, data->aircraft_icao, 0, 40);
-    if (data->aircraft_icao == NULL)
-        goto cleanup;
-
-    XPLMGetDatab(aircraft_tailnum_dr, data->aircraft_tailnum, 0, 40);
-    if (data->aircraft_tailnum == NULL)
-        goto cleanup;
-
-    XPLMGetDatab(aircraft_description_dr, data->aircraft_description, 0, 260);
-    if (data->aircraft_description == NULL)
-        goto cleanup;
-
-    data->latitude = XPLMGetDatad(latitude_dr);
-    data->longitude = XPLMGetDatad(longitude_dr);
-    data->altitude = XPLMGetDatad(altitude_dr);
-    data->track = XPLMGetDataf(track_dr);
-    data->ground_speed = XPLMGetDataf(ground_speed_dr);
-    data->air_speed = XPLMGetDataf(air_speed_dr);
-    data->vertical_speed = XPLMGetDataf(vertical_speed_dr);
-
     // FIXME: make this global, and increment with retries
-    float time = 3;  // in seconds
+    float time = 15;  // in seconds
 
-    if (!SendPositionData(str_strip(url), data)) {
-        // delay next cycle in 10s, to give the server a bit more time to
-        // recover
-        time += 10;
+    if (flight_id == -1) {
+        flight_data_t flight_data;
+        XPLMGetDatab(aircraft_icao_dr, flight_data.aircraft_icao, 0, 40);
+        if (flight_data.aircraft_icao == NULL)
+            return time;
+
+        XPLMGetDatab(aircraft_tailnum_dr, flight_data.aircraft_tailnum, 0, 40);
+        if (flight_data.aircraft_tailnum == NULL)
+            return time;
+
+        XPLMGetDatab(aircraft_description_dr, flight_data.aircraft_description,
+            0, 260);
+        if (flight_data.aircraft_description == NULL)
+            return time;
+
+        if (!SendFlightData(str_strip(url), &flight_data) || flight_id == -1) {
+            XPLMSetMenuItemName(status_menu, status_index,
+                "Status: Initialization failed", 1);
+            return time;
+        }
+        sending = false;
+    }
+
+    position_data_t data;
+    data.latitude = XPLMGetDatad(latitude_dr);
+    data.longitude = XPLMGetDatad(longitude_dr);
+    data.altitude = XPLMGetDatad(altitude_dr);
+    data.track = XPLMGetDataf(track_dr);
+    data.ground_speed = XPLMGetDataf(ground_speed_dr);
+    data.air_speed = XPLMGetDataf(air_speed_dr);
+    data.vertical_speed = XPLMGetDataf(vertical_speed_dr);
+
+    if (!SendPositionData(str_strip(url), &data)) {
         if (sending) {
             XPLMSetMenuItemName(status_menu, status_index, "Status: Failed", 1);
             sending = false;
+            return time;
         }
     }
     else if (!sending) {
@@ -222,9 +335,7 @@ FlightLoopCallback(float elapsedMe, float elapsedSim, int counter, void *refcon)
         sending = true;
     }
 
-cleanup:
-    free(data);
-    return time;
+    return 3;
 }
 
 
@@ -324,8 +435,8 @@ XPluginEnable(void)
     status_menu = XPLMCreateMenu("xplogd", XPLMFindPluginsMenu(),
         main_submenu, NULL, 0);
 
-    sending = true;
-    status_index = XPLMAppendMenuItem(status_menu, "Status: Ok", NULL, 1);
+    status_index = XPLMAppendMenuItem(status_menu, "Status: Initializing",
+        NULL, 1);
     XPLMEnableMenuItem(status_menu, status_index, 0);
 
     XPLMRegisterFlightLoopCallback(FlightLoopCallback, -1, 0);
